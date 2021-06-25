f53d9445b44075629c444b3ca5c169a03b674967
Jesús Vargas <jesus.vargas@jeegox.com>
2021-06-24 22:14:29 -0500

    decouple the tests from the real reading of the suburbs using mockito.

--- src/test/java/com/j2esus/sepomex/repositories/StateRepositoryTest.java
+++ src/test/java/com/j2esus/sepomex/repositories/StateRepositoryTest.java
@@ -1,7 +1,12 @@
 package com.j2esus.sepomex.repositories;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import com.j2esus.sepomex.model.Suburb;
 import com.j2esus.sepomex.util.Datasource;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
@@ -13,8 +18,82 @@ public class StateRepositoryTest {
 
   @BeforeAll
   public static void beforeAll(){
-    datasource = new Datasource();
+    datasource = mock(Datasource.class);
+    when(datasource.getSuburbs()).thenReturn(someSuburbs());
     stateRepository = new StateRepository(datasource);
+  }
+
+  private static List<Suburb> someSuburbs(){
+    List<Suburb> suburbs = new ArrayList<>();
+    suburbs.add(
+      new Suburb("01000","San Ángel", "Colonia", "Álvaro Obregón", "Ciudad de México"));
+    suburbs.add(
+      new Suburb("01010","Los Alpes", "Colonia", "Álvaro Obregón", "Ciudad de México"));
+    suburbs.add(
+      new Suburb("20000","Zona Centro", "Colonia", "Aguascalientes", "Aguascalientes"));
+    suburbs.add(
+      new Suburb("21000","Centro Cívico", "Colonia", "Mexicali", "Baja California"));
+    suburbs.add(
+      new Suburb("23000","Zona Centro", "Colonia", "La Paz", "Baja California Sur"));
+    suburbs.add(
+      new Suburb("24000","San Francisco de Campeche Centro", "Colonia", "Campeche", "Campeche"));
+    suburbs.add(
+      new Suburb("29000","Guadalupe", "Barrio", "Tuxtla Gutiérrez", "Chiapas"));
+    suburbs.add(
+      new Suburb("31000","Colon", "Colonia", "Chihuahua", "Chihuahua"));
+    suburbs.add(
+      new Suburb("25000","Saltillo Zona Centro", "Colonia", "Saltillo", "Coahuila de Zaragoza"));
+    suburbs.add(
+      new Suburb("28000","Colima Centro", "Colonia", "Colima", "Colima"));
+    suburbs.add(
+      new Suburb("34010","Morga", "Colonia", "Durango", "Durango"));
+    suburbs.add(
+      new Suburb("36000","Guanajuato Centro", "Colonia", "Guanajuato", "Guanajuato"));
+    suburbs.add(
+      new Suburb("39010","Caminos", "Colonia", "Chilpancingo de los Bravo", "Guerrero"));
+    suburbs.add(
+      new Suburb("42119","Las Torres", "Fraccionamiento", "Pachuca de Soto", "Hidalgo"));
+    suburbs.add(
+      new Suburb("44150","Barrera", "Colonia", "Guadalajara", "Jalisco"));
+    suburbs.add(
+      new Suburb("58020","Cuauhtémoc", "Colonia", "Morelia", "Michoacán de Ocampo"));
+    suburbs.add(
+      new Suburb("62563","Sumiya", "Fraccionamiento", "Jiutepec", "Morelos"));
+    suburbs.add(
+      new Suburb("50450","Centro", "Colonia", "Atlacomulco", "México"));
+    suburbs.add(
+      new Suburb("63000","Tepic Centro", "Colonia", "Tepic", "Nayarit"));
+    suburbs.add(
+      new Suburb("64000","Monterrey Centro", "Colonia", "Monterrey", "Nuevo León"));
+    suburbs.add(
+      new Suburb("68418","Sagrado Corazón", "Colonia", "Cosolapa", "Oaxaca"));
+    suburbs.add(
+      new Suburb("72000","Centro", "Colonia", "Puebla", "Puebla"));
+    suburbs.add(
+      new Suburb("76000","Centro", "Colonia", "Querétaro", "Querétaro"));
+    suburbs.add(
+      new Suburb("77000","Chetumal Centro", "Colonia", "Othón P. Blanco", "Quintana Roo"));
+    suburbs.add(
+      new Suburb("78000","San Luis Potosí Centro", "Colonia", "San Luis Potosí", "San Luis Potosí"));
+    suburbs.add(
+      new Suburb("80000","Centro Sinaloa", "Colonia", "Culiacán", "Sinaloa"));
+    suburbs.add(
+      new Suburb("83000","Centro Norte", "Colonia", "Hermosillo", "Sonora"));
+    suburbs.add(
+      new Suburb("86020","Framboyanes", "Colonia", "Centro", "Tabasco"));
+    suburbs.add(
+      new Suburb("87000","Ciudad Victoria Centro", "Colonia", "Victoria", "Tamaulipas"));
+    suburbs.add(
+      new Suburb("90000","Tlaxcala Centro", "Colonia", "Tlaxcala", "Tlaxcala"));
+    suburbs.add(
+      new Suburb("91013","Solidaridad", "Colonia", "Xalapa", "Veracruz de Ignacio de la Llave"));
+    suburbs.add(
+      new Suburb("97000","Los Cocos", "Fraccionamiento", "Mérida", "Yucatán"));
+    suburbs.add(
+      new Suburb("98000","Zacatecas Centro", "Colonia", "Zacatecas", "Zacatecas"));
+    suburbs.add(
+      new Suburb("98024","La Pinta", "Colonia", "Zacatecas", "Zacatecas"));
+    return suburbs;
   }
 
   @Test
@@ -23,7 +102,7 @@ public class StateRepositoryTest {
     Assertions.assertEquals(expectedStates(), states);
   }
 
-  public Set<String> expectedStates(){
+  private Set<String> expectedStates(){
     Set<String> states = new TreeSet<>();
     states.add("Aguascalientes");
     states.add("Baja California");
@@ -66,7 +145,7 @@ public class StateRepositoryTest {
     Assertions.assertEquals(expectedByNameStates(), states);
   }
 
-  public Set<String> expectedByNameStates(){
+  private Set<String> expectedByNameStates(){
     Set<String> states = new TreeSet<>();
     states.add("Baja California");
     states.add("Baja California Sur");
@@ -79,7 +158,7 @@ public class StateRepositoryTest {
     Assertions.assertEquals(expectedByNameState(), states);
   }
 
-  public Set<String> expectedByNameState(){
+  private Set<String> expectedByNameState(){
     Set<String> states = new TreeSet<>();
     states.add("Guerrero");
     return states;
