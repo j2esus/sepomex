e750a680ec36afcf6202a6a9f2700e0ebb5d1e7f
Jesús Vargas <jesus.vargas@jeegox.com>
2021-07-11 21:05:09 -0500

    Change the url for getting towns

--- src/main/java/com/j2esus/sepomex/controllers/TownController.java
+++ src/main/java/com/j2esus/sepomex/controllers/TownController.java
@@ -1,19 +1,17 @@
 package com.j2esus.sepomex.controllers;
 
 import java.util.Set;
-
 import com.j2esus.sepomex.model.Town;
 import com.j2esus.sepomex.repositories.TownRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/sepomex/api")
+@RequestMapping("/sepomex/v1/")
 public class TownController {
   private final TownRepository townRepository;
 
@@ -22,8 +20,9 @@ public class TownController {
     this.townRepository = townRepository;
   }
 
-  @GetMapping("/municipios/estado/nombre/{state}")
-  public ResponseEntity<Set<Town>> towns(@PathVariable String state){
+  @GetMapping("municipios")
+  public ResponseEntity<Set<Town>> towns(
+      @RequestParam(value = "estado") String state){
     return ResponseEntity.ok(townRepository.findByState(state));
   }
 }
