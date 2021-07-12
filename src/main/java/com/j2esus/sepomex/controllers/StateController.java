c678920f91374b4e62354f7381f20f23ecb0493d
Jesús Vargas <jesus.vargas@jeegox.com>
2021-07-11 20:57:23 -0500

    Change the url for getting states

--- src/main/java/com/j2esus/sepomex/controllers/StateController.java
+++ src/main/java/com/j2esus/sepomex/controllers/StateController.java
@@ -2,15 +2,17 @@ package com.j2esus.sepomex.controllers;
 
 import java.util.Set;
 import com.j2esus.sepomex.repositories.StateRepository;
+
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/sepomex/api")
+@RequestMapping("/sepomex/v1/")
 public class StateController {
     private final StateRepository stateRepository;
 
@@ -19,13 +21,10 @@ public class StateController {
         this.stateRepository = stateRepository;
     }
 
-    @GetMapping("/estados")
-    public ResponseEntity<Set<String>> states(){
-        return ResponseEntity.ok(stateRepository.getAllStates());
-    }
-
-    @GetMapping("/estados/nombre/{name}")
-    public ResponseEntity<Set<String>> states(@PathVariable String name){
-        return ResponseEntity.ok(stateRepository.findByName(name));
+    @GetMapping("estados")
+    public ResponseEntity<Set<String>> states(
+        @RequestParam(value = "nombre", defaultValue =  "") String name){
+        return ResponseEntity.ok(Strings.isEmpty(name) ?
+            stateRepository.getAllStates() : stateRepository.findByName(name));
     }
 }
