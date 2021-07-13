a3fc3bd664bf4f11d3a8a15a29338d26d75f9219
Jesús Vargas <jesus.vargas@jeegox.com>
2021-07-12 21:41:23 -0500

    Change the url for getting suburbs

--- src/main/java/com/j2esus/sepomex/controllers/SuburbController.java
+++ src/main/java/com/j2esus/sepomex/controllers/SuburbController.java
@@ -1,19 +1,18 @@
 package com.j2esus.sepomex.controllers;
 
 import java.util.List;
-
 import com.j2esus.sepomex.model.Suburb;
 import com.j2esus.sepomex.repositories.SuburbRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/sepomex/api")
+@RequestMapping("/sepomex/v1/")
 public class SuburbController {
   private final SuburbRepository suburbRepository;
 
@@ -22,15 +21,15 @@ public class SuburbController {
     this.suburbRepository = suburbRepository;
   }
 
-  @GetMapping("/colonias/codigoPostal/{zipCode}")
+  @GetMapping("colonias/codigo-postal/{zipCode}")
   public ResponseEntity<List<Suburb>> suburbs(@PathVariable String zipCode){
     return ResponseEntity.ok(suburbRepository.findByZipCode(zipCode));
   }
 
-  @GetMapping("/colonias/estado/nombre/{state}/municipio/nombre/{town}")
-  public ResponseEntity<List<Suburb>> suburbs(@PathVariable String state,
-    @PathVariable String town){
+  @GetMapping("colonias")
+  public ResponseEntity<List<Suburb>> suburbs(
+    @RequestParam(value = "estado") String state,
+    @RequestParam(value = "municipio") String town){
     return ResponseEntity.ok(suburbRepository.findByStateAndTown(state, town));
   }
-  
 }
